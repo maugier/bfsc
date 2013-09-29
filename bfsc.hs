a14994ba6e8eb83b76414e2f25255cbc577d0b0e
Maxime Augier <max@xolus.net>
2013-09-29 22:00:43 +0200

    Cost management, other opts

--- bfsc.hs
+++ bfsc.hs
@@ -3,6 +3,8 @@ import Data.Char
 import Data.List
 import Data.Word
 import Control.Monad.RWS
+import Data.Ord (comparing)
+import qualified Data.Map as M
 
 data BF = MLeft | MRight | Up | Down | Out | Loop [BF]
 	deriving Eq
@@ -35,8 +37,10 @@ instance Read BF where
 
 data BFMachine = BFMachine [Word8] [Word8]
 
+showBy s = concat . intersperse s . map show
+
 instance Show BFMachine where
-	show (BFMachine (x:l) r) = concatMap show (reverse l) ++ "[" ++ show x ++ "]" ++ concatMap show r
+	show (BFMachine (x:l) r) = showBy "|" (reverse l) ++ "[" ++ show x ++ "]" ++ showBy "|"  r
 
 initBF = BFMachine [0] []
 
@@ -45,16 +49,16 @@ type BFExec = RWS () Char BFMachine
 state_ f = state $ \s -> ((), f s)
 peek = rws (\() s@(BFMachine (x:_) _) -> (x,s,""))
 
-left (BFMachine [x]   r) = BFMachine [0] (x:r)
-left (BFMachine (x:l) r) = BFMachine l   (x:r)
+iLeft (BFMachine [x]   r) = BFMachine [0] (x:r)
+iLeft (BFMachine (x:l) r) = BFMachine l   (x:r)
 
-right (BFMachine l [])    = BFMachine (0:l) []
-right (BFMachine l (x:r)) = BFMachine (x:l) r
+iRight (BFMachine l [])    = BFMachine (0:l) []
+iRight (BFMachine l (x:r)) = BFMachine (x:l) r
 
 execMany = mapM_ exec
 
-exec MLeft  = state_ $ left
-exec MRight = state_ $ right
+exec MLeft  = state_ $ iLeft
+exec MRight = state_ $ iRight
 exec Up    = state_ $ \(BFMachine (x:l) r) -> BFMachine ((x+1):l) r
 exec Down  = state_ $ \(BFMachine (x:l) r) -> BFMachine ((x-1):l) r
 exec Out   = peek >>= \x -> writer ((), [chr (fromIntegral x)])
@@ -64,16 +68,56 @@ exec l@(Loop prog) = do
 		0 -> return ()
 		_ -> execMany prog >> exec l
 
+runBF bf = execRWS (execMany (code bf)) () initBF
 
-runBF bf = execRWS (execMany bf) () initBF
+data BFProg = BFProg { cost :: Int, code :: [BF] }
+	deriving Eq
 
-reset :: [BF]
+instance Read BFProg where
+	readsPrec i s = [ (bfprog p, r) | (p,r) <- readsPrec i s ]
+
+instance Show BFProg where
+	show = show . code
+
+bfprog bf = BFProg (ccost bf) bf where
+	ccost = sum . map icost
+	icost (Loop p) = ccost p + 2
+	icost _        = 1
+
+instance Monoid BFProg where
+	mempty = BFProg 0 []
+	mappend (BFProg l1 p1) (BFProg l2 p2) = BFProg (l1+l2) (p1++p2)
+
+instance Ord BFProg where
+	compare = comparing $ \(BFProg l _) -> l
+
+reset :: BFProg
 reset = read "[-]"
 
-linear :: Word8 -> [BF]
-linear x = replicate (fromIntegral x) Up
+stop :: BFProg
+stop = bfprog []
+
+left = bfprog [MLeft]
+right = bfprog [MRight]
+out = bfprog [Out]
+inc = bfprog [Up]
+dec = bfprog [Down]
+loop = bfprog . (:[]) . Loop
 
 
+linear :: Word8 -> BFProg
+linear x | x > 127   = bfprog (replicate (fromIntegral $ 256 - x ) Down)
+         | otherwise = bfprog (replicate (fromIntegral x) Up)
+
+
+mult a = bfprog [Loop ((MRight : code (linear a)) ++ [MLeft,Down]), MRight] 
+--mult a = right <> linear a <> loop ( left 
+
+linears = [ (x, linear x) | x <- [0 .. 255] ]
+
+affines = [ (a*b+c, linear a <> mult b <> linear c) | a <- [2..16], b <- [a..255], c <- [0..a] ]
+
+best = M.fromListWith min (linears ++ affines)
 
 identities :: [BF] -> [BF]
 
@@ -84,6 +128,11 @@ identities (MRight:MLeft:rest) = identities rest
 identities (x:rest) = x : identities rest
 identities [] = []
 
+cuthead (MLeft: rest)  = cuthead rest
+cuthead (MRight: rest) = cuthead rest
+cuthead rest           = rest
+
 efix f = head . head . dropWhile (\(a:b:_) -> a /= b) . tails . iterate f
 
-optimize = efix identities 
+optimize = cuthead . efix identities 
+
