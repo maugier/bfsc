6203ff58930faa1ac405080fdc9b079d6530ae16
Maxime Augier <max@xolus.net>
2014-08-18 17:41:07 +0200

    symbolicExec helper and type signatures

--- BFSC/SymbolicExec.hs
+++ BFSC/SymbolicExec.hs
@@ -17,12 +17,13 @@ type VarBinds = Map String (Either Word8 Word8)
 
 type SymbolicState = (VarBinds, BFMachine Cell)
 
-type SymbolicRun = RWST () String SymbolicState []
+type SymbolicRun = RWST () [Cell] SymbolicState []
 
 onMachine = state_ . second
 
 execMany = mapM_ exec
 
+exec :: BF -> SymbolicRun ()
 exec MLeft = onMachine iLeft
 exec MRight = onMachine iRight
 exec Up = onMachine $ onPtr (+1)
@@ -30,3 +31,6 @@ exec Down = onMachine $ onPtr (subtract 1)
 exec (Loop [Down]) = onMachine $ onPtr (const 0)
 exec (Loop [Up])   = onMachine $ onPtr (const 0)
 exec _             = error "Unsupported program for symbolic execute"
+
+symbolicExec :: [BF] -> BFMachine Cell -> [(SymbolicState, [Cell])]
+symbolicExec program init = execRWST (execMany program) () (empty, init)
