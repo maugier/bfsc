f0743d94da8488a4563ceab7fb6a400ae2187ef7
Maxime Augier <max@xolus.net>
2014-08-18 14:11:05 +0200

    Start implementing symbolic expressions

---
+++ BFSC/SymbolicExec.hs
@@ -0,0 +1,39 @@
+{-
+ - Symbolic Execution module
+ -}
+
+
+import BFSC.Base
+import Data.Map
+import Data.Word
+import Data.List
+
+newtype Var f = Var (Map [String] f)
+
+constant :: t -> Var t
+constant x = Var (singleton [] x)
+
+var :: Num t => String -> Var t
+var s = Var (singleton [s] 1)
+
+instance (Eq t, Num t) => Num (Var t) where
+    fromInteger = constant . fromInteger
+    negate (Var x) = Var (fmap negate x)
+    Var a + Var b = Var (Data.Map.filter (/= 0) $ unionWith (+) a b)
+    Var a * Var b = Var $ fromListWith (+) [ (sort (ka ++ kb), va*vb) | (ka,va) <- toList a, (kb,vb) <- toList b ]
+
+instance (Show t, Num t, Eq t) => Show (Var t) where
+    show (Var x) = concat $ intersperse "+" [ (showMonom v k) | (k,v) <- toList x ]
+
+showMonom v [] = show v
+showMonom 1 ks = concat $ intersperse "*" ks
+showMonom v ks = concat $ intersperse "*" (show v : ks)
+
+type Cell = Var Word8
+
+data SymbolicMachine = SymbolicMachine [Cell] [Cell]
+
+initSM = SymbolicMachine [0] []
+
+--type SymbolicExec = RWS () [Var] SymbolicMachine
+
