cc3ddc90e65917f039c1abce2ff5f298fac3ee44
Maxime Augier <max@xolus.net>
2014-08-21 15:32:33 +0200

    Basic symbolic execution working

--- BFSC/Expr.hs
+++ BFSC/Expr.hs
@@ -8,8 +8,13 @@ import Data.Map
 import Data.Maybe (isJust)
 import Data.List
 
+import Control.Monad
+
 newtype Var f = Var (Map [String] f)
     deriving (Eq, Ord)
+
+mkVar :: (Num t, Eq t) => Map [String] t -> Var t
+mkVar = Var . Data.Map.filter (/= 0)
 
 constant :: t -> Var t
 constant x = Var (singleton [] x)
@@ -27,16 +32,16 @@ isConstant = isJust . toConstant
 var :: Num t => String -> Var t
 var s = Var (singleton [s] 1)
 
-substitute :: Num t => String -> t -> Var t -> Var t
-substitute var val (Var x) = Var . fromListWith (+) . fmap subst . toList $ x where
+substitute :: (Num t, Eq t) => String -> t -> Var t -> Var t
+substitute var val (Var x) = mkVar . fromListWith (+) . fmap subst . toList $ x where
     subst (ks, v) = (others, v * product (replicate (length good) val)) where
         (good, others) = Data.List.partition (var ==) ks
 
 instance (Eq t, Num t) => Num (Var t) where
     fromInteger = constant . fromInteger
     negate (Var x) = Var (fmap negate x)
-    Var a + Var b = Var (Data.Map.filter (/= 0) $ unionWith (+) a b)
-    Var a * Var b = Var $ fromListWith (+) [ (sort (ka ++ kb), va*vb) | (ka,va) <- toList a, (kb,vb) <- toList b ]
+    Var a + Var b = mkVar $ unionWith (+) a b
+    Var a * Var b = mkVar $ fromListWith (+) [ (sort (ka ++ kb), va*vb) | (ka,va) <- toList a, (kb,vb) <- toList b ]
     abs = undefined
     signum = undefined
 
@@ -52,3 +57,16 @@ showMonom v ks = concat $ intersperse "*" (show v : ks)
 
 freeVariables :: Var t -> [String]
 freeVariables (Var x) = nub . sort . concat . keys $ x
+
+
+{- Retourne une liste de substitutions possibles si la contrainte est satisfaite. 
+ - Nothing représente aucune substitution; en termes opérationnels, Nothing
+ - doit être considéré comme équivalent à (Just id).
+ - -}
+solveSimple :: (Eq t, Num t, MonadPlus m) => Var t -> m (Maybe (String, t))
+solveSimple (Var x) = case toAscList x of
+    [([], v)] | v /= 0  -> mzero      -- x = 0 pour x != 0, impossible, bailout
+    [([], k), ([var], 1)] -> return (Just (var, negate k))
+    [([], k), ([var], 255)] -> return (Just (var, k))
+    _ -> return Nothing
+    
--- BFSC/SymbolicExec.hs
+++ BFSC/SymbolicExec.hs
@@ -8,6 +8,7 @@ import BFSC.Machine
 
 import Control.Monad.RWS
 import Control.Arrow
+import Data.Char (chr)
 import Data.Word
 import Data.Map
 
@@ -15,22 +16,44 @@ type Cell = Var Word8
 
 {- Sets of allowable values -}
 type Constraints = Map Cell Bool
-type SymbolicState = (Constraints, BFMachine Cell)
+type Varbinds    = Map String Word8
+data SymbolicState = SymbolicState { syConstraints :: Constraints,
+                                     syVarbinds    :: Varbinds,
+                                     syMachine     :: BFMachine Cell }
+    deriving Show
 
 
 type SymbolicRun = RWST () [Cell] SymbolicState []
 
-onMachine = state_ . second
-onBinds = state_ . first
+onMachine f     = state_ (\(SymbolicState c v m) -> SymbolicState c v (f m))
+onBinds f       = state_ (\(SymbolicState c v m) -> SymbolicState c (f v) m)
+onConstraints f = state_ (\(SymbolicState c v m) -> SymbolicState (f c) v m)
 
+
+bind :: String -> Word8 -> SymbolicRun ()
+bind var val = do
+    onBinds (insert var val)
+    onMachine (fmap (substitute var val))
 
 assert :: Cell -> Bool -> SymbolicRun ()
-assert cell val = onBinds (insert cell val)
+assert cell True = onConstraints (insert cell True) 
+assert cell False = do  
+    sols <- solveSimple cell
+    case sols of
+        Nothing -> return ()
+        Just (var,val) -> bind var val
+
+
+
+{-
+assert cell val = do 
+                | otherwise = onBinds (insert cell val) -}
 
 
 execMany = mapM_ exec
 
 exec :: BF -> SymbolicRun ()
+exec Out = do { st <- get; tell [getPtr (syMachine st)] }
 exec MLeft = onMachine iLeft
 exec MRight = onMachine iRight
 exec Up = onMachine $ onPtr (+1)
@@ -42,8 +65,8 @@ exec (Loop loop) | isLinear loop = execLinearLoop loop
 
 execLoop :: [BF] -> SymbolicRun ()
 execLoop loop = do
-    (_, machine) <- get
-    let ptr = getPtr machine
+    st <- get
+    let ptr = getPtr (syMachine st)
     case toConstant ptr of
         Just 0 -> return () -- loop is known not to execute
         Just _ -> execMany loop >> execLoop loop -- loop is known to execute
@@ -51,13 +74,14 @@ execLoop loop = do
 
 execLinearLoop :: [BF] -> SymbolicRun ()
 execLinearLoop loop = do
-    let [((_,result), _)] = symbolicExec loop initBF
-    machine <- fmap snd get
+    let [(st, _)] = symbolicExec loop initBF
+    let result = syMachine st
+    machine <- fmap syMachine get
     let cur = getPtr machine :: Cell
     onMachine $ (|+| fmap (* cur) result)
 
 symbolicExec :: [BF] -> BFMachine Cell -> [(SymbolicState, [Cell])]
-symbolicExec program init = execRWST (execMany program) () (empty, init)
+symbolicExec program init = execRWST (execMany program) () (SymbolicState empty empty init)
 
 linearOp MLeft = True
 linearOp MRight = True
